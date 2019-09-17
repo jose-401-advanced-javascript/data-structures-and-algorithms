b910467dd8d70d780f39e737e2e874b5f7207da2
jaojeda <j.a.ojeda@live.com>
2019-09-17 08:26:26 -0700

    corrected function to return array

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -2,12 +2,13 @@
 const reverseArray = array => {
     let first = '';
     let last = '';
-    for(let i = 0; i <= array.length / 2; i++) {
+    for(let i = 0; i <= Math.floor(array.length / 2); i++) {
         first = array[i];
         last = array[array.length - 1 - i];
         array[array.length - 1 - i] = first;
         array[i] = last;
     }
+    return array;
 }
 
 module.exports = {
