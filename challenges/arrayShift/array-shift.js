b68c5f8b819eb02a8f2b5518b1f8e2c9fa4197c9
jaojeda <j.a.ojeda@live.com>
2019-09-21 18:31:50 -0700

    finished testing array shift

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,10 +1,10 @@
 
 const addFirst = (arr, num) => {
-    for(let i = arr.length; i > 0; i--){
-        arr[i] = arr[i - 1];
-    }
-   arr[0] = num;
-   return arr;
-}
+  for(let i = arr.length; i > 0; i--){
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = num;
+  return arr;
+};
 
-module.exports = {addFirst};
+module.exports = { addFirst };
