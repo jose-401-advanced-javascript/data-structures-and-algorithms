01a0ea20f48bef4004d57d08232ce5fa45020d10
jaojeda <j.a.ojeda@live.com>
2019-09-22 17:24:53 -0700

    created challenge readme, added wb, wrote starting function

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,27 @@
+
+const findNum = (arr, num) => {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while(low <= high){
+    let mid = Math.floor((low + high) / 2);
+
+    if(num === arr[mid]){
+      return mid;
+    }
+
+    else if(num > arr[mid]){
+      low = mid + 1;
+    }
+
+    else if(num < arr[mid]){
+      high = mid - 1;
+    }
+
+    else {
+      return -1;
+    }
+  }
+};
+
+module.exports = { findNum };
