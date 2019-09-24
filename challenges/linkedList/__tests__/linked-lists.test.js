36c4274d5f8cc4bb2c85230ebd6121745d340619
jaojeda <j.a.ojeda@live.com>
2019-09-23 23:11:47 -0700

    updated test for better accuracy, tested for proper order

--- challenges/linkedList/__tests__/linked-lists.test.js
+++ challenges/linkedList/__tests__/linked-lists.test.js
@@ -1,4 +1,5 @@
 const { LinkedList } = require('../linked-list');
+const { Node } = require('../node-class');
 
 describe('Linked list', () => {
 
@@ -14,15 +15,24 @@ describe('Linked list', () => {
 
     const list = new LinkedList();
     list.insert('test');
-    const obj = {
-      head: { 
-        'next': null,
-        'value': 'test'
-      } 
-    };
+    list.insert('test-2');
+ 
     it('inserts node into linked list', () => {
-      expect(list).toEqual(obj);
+      expect(list.head).toBeInstanceOf(Node);
+      expect(list.head.value).toBe('test');
     });
+
+    it('properly insert multiple nodes into the linked list', () => {
+      expect(list.head.next).toBeInstanceOf(Node);
+      expect(list.head.next.value).toBe('test-2');    
+    });
+
+    it('head property will properly point to the first node in the linked list', () => {
+      expect(list.head).toBeInstanceOf(Node);
+      expect(list.head.value).toBe('test');
+      expect(list.head.next.next).toBe(null);
+    });
+
   });
 });
 
